51de177ac243a7b52c7767e49b031cd3ca9d1aaf
Valentina Malkova <moonly@Valentinas-MacBook-Air.local>
2025-05-14 15:38:22 +0300

    Додано код в 10.3.3

--- 10.3.3.py
+++ 10.3.3.py
@@ -1,1 +1,11 @@
+def log_function_name(func):
+    def wrapper(*args, **kwargs):
+        print(f"Викликається функція: {func.__name__}")
+        return func(*args, **kwargs)
+    return wrapper
 
+@log_function_name
+def example_function():
+    print("Це приклад функції")
+
+example_function()
