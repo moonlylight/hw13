e8d6cf38c022b1accbf7285607a5ddcf6b69dd5c
Valentina Malkova <moonly@Valentinas-MacBook-Air.local>
2025-05-14 15:39:27 +0300

    Додано код в 10.3.4

--- 10.3.4.py
+++ 10.3.4.py
@@ -1,1 +1,13 @@
+def class_logger(cls):
+    class Wrapped(cls):
+        def __init__(self, *args, **kwargs):
+            print(f"Створюється екземпляр класу {cls.__name__}")
+            super().__init__(*args, **kwargs)
+    return Wrapped
 
+@class_logger
+class ExampleClass:
+    def __init__(self, value):
+        self.value = value
+
+instance = ExampleClass(42)
