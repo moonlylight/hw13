434caaa26a8c8bacd691e3a2e8a09498a5dbbde4
Valentina Malkova <moonly@Valentinas-MacBook-Air.local>
2025-05-14 15:51:01 +0300

    Оновлено код та виведено результати в окремий файл

--- 10.3.4.py
+++ 10.3.4.py
@@ -1,7 +1,8 @@
 def class_logger(cls):
     class Wrapped(cls):
         def __init__(self, *args, **kwargs):
-            print(f"Створюється екземпляр класу {cls.__name__}")
+            with open("output4.txt", "w", encoding="utf-8") as file:
+                file.write(f"Створюється екземпляр класу {cls.__name__}\n")
             super().__init__(*args, **kwargs)
     return Wrapped
 
@@ -11,3 +12,5 @@ class ExampleClass:
         self.value = value
 
 instance = ExampleClass(42)
+
+print("Результати записані у output4.txt")
