e3918d6f723a2b28a67f421bfd7eb0a8d46e55ac
Valentina Malkova <moonly@Valentinas-MacBook-Air.local>
2025-05-14 15:37:02 +0300

    Додано код в 10.3.2

--- 10.3.2.py
+++ 10.3.2.py
@@ -1,1 +1,12 @@
+class DynamicClass:
+    def __init__(self, name):
+        self.name = name
 
+    def greet(self):
+        return f"Hello, {self.name}!"
+
+attributes = {"name": "Python", "greet": lambda self: f"Hello, {self.name}!"}
+NewClass = type("NewClass", (object,), attributes)
+
+instance = NewClass()
+print(instance.greet())
