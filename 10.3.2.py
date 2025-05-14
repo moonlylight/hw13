4698b4e9f75af7bb2b3792d48d4c24bf15785ce3
Valentina Malkova <moonly@Valentinas-MacBook-Air.local>
2025-05-14 15:46:54 +0300

    Оновлено код та виведено результати в окремий файл

--- 10.3.2.py
+++ 10.3.2.py
@@ -9,4 +9,8 @@ attributes = {"name": "Python", "greet": lambda self: f"Hello, {self.name}!"}
 NewClass = type("NewClass", (object,), attributes)
 
 instance = NewClass()
-print(instance.greet())
+
+with open("output2.txt", "w", encoding="utf-8") as file:
+    file.write(instance.greet() + "\n")
+
+print("Результати записані у output2.txt")
